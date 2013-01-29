c3a50495420d2740e846656f4ac778c04a2476bb
krasidimitrov <krasimir.dimitrov@clouway.com>
2013-01-30 00:44:05 +0200

    blq

--- src/com/easybanking/server/PersistenceAccountBase.java
+++ src/com/easybanking/server/PersistenceAccountBase.java
@@ -16,48 +16,13 @@ public class PersistenceAccountBase implements AccountBase {
 
 
   private final Provider<ObjectDatastore> datastore;
+  private final Provider<User> currentUser;
 
   @Inject
-  public PersistenceAccountBase(Provider<ObjectDatastore> datastore) {
+  public PersistenceAccountBase(Provider<ObjectDatastore> datastore, Provider<User> currentUser) {
     this.datastore = datastore;
+    this.currentUser = currentUser;
   }
 
-  public void createTestData() {
 
-  }
-
-  public List<String> getAccountNumbersByUser(String user) {
-//    Query query = new Query("Account");
-//    query.setAncestor(KeyFactory.createKey("User",user));
-//    query.addProjection(new PropertyProjection("number", String.class));
-//
-//    PreparedQuery preparedQuery = datastoreService.prepare(query);
-//    return convertToStringList(preparedQuery.asList(FetchOptions.Builder.withDefaults()));
-    return Lists.newArrayList("abc123bg","bbb321gg");
-  }
-
-  public Account getAccount(String username, String number) {
-//    Query query = new Query("Account");
-////    query.setAncestor(KeyFactory.createKey("User", username));
-//    query.setFilter(new Query.FilterPredicate("number", Query.FilterOperator.EQUAL,number));
-//    PreparedQuery preparedQuery = datastoreService.prepare(query);
-//    return convertToAccountObject(preparedQuery.asSingleEntity());
-     return new Account("abc123bg", 12, "bgn", 3);
-  }
-
-  private List<String> convertToStringList(List<Entity> listOfEntities) {
-    List<String> listOfStrings = new ArrayList<String>();
-    for (Entity entity : listOfEntities) {
-      listOfStrings.add((String) entity.getProperty("number"));
-    }
-    return listOfStrings;
-  }
-
-  private Account convertToAccountObject(Entity entity){
-    String number = String.valueOf(entity.getProperty("number"));
-    double balance = Double.parseDouble(String.valueOf(entity.getProperty("balance")));
-    String currency = String.valueOf(entity.getProperty("currency"));
-    double interest = Double.parseDouble(String.valueOf(entity.getProperty("interest")));
-    return new Account(number,balance,currency,interest);
-  }
 }
